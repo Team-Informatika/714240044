373d5cecb669ea3a6bd07bcd6710914fb93b5f30
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2025-01-31 18:07:42 +0700

    coba

--- data.js
+++ data.js
@@ -13,22 +13,6 @@ function renderDataDariJson() {
 }
 
 function responseFunction(data) {
-    console.log(data);
-    let apaja;
-    apaja = data.data;
-    const container = document.getElementById("data");
-    apaja.forEach((item) => {
-        const data = document.createElement("div");
-        layananItem.classList.add("card");
-        const Name = document.createElement("h1");
-        Name.textContent = item.Name;
-
-        const Ug = document.createElement("p");
-        Ug.textContent = item.Ug;
-
-        data.appendChild(Name);
-        data.appendChild(Ug);
-        container.appendChild(data)
-    });
+    setInner("konten", file.data.Name);
 
 }
