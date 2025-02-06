fc72bbc2084af99edbe4ff20c4dcff2eacd395f3
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2025-02-06 11:40:16 +0700

    coba

--- data.js
+++ data.js
@@ -14,12 +14,6 @@ function responseFunction(isi) {
     console.log(isi);
 
 
-    setInner("konten", isi.data.
-        Foto,
-        Name,
-        Ug,
-        Skill,
-        Rate
-
-    );
+    setInner("konten", isi.data.Name);
+    setInner("konten", isi.data.Ug);
 }
