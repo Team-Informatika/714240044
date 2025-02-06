edce3665be2fab30645560006824695a442705b4
MuhRifa2024 <muhamad.rifaldi174@gmail.com>
2025-02-06 12:09:16 +0700

    coba

--- data.js
+++ data.js
@@ -12,10 +12,13 @@ function renderDataDariJson() {
 
 function responseFunction(isi) {
     console.log(isi);
+    const avatarSrc = isi.data.Foto;
+    const avatarHTML = <img src="${FotoSrc}" alt="Avatar">;
+        setInner('avatar', avatarHTML);
 
 
-    setInner("konten", isi.data.Name);
-    setInner("ug", isi.data.Ug);
-    setInner("skill", isi.data.Skill);
-    setInner("rate", isi.data.Rate);
+        setInner("konten", isi.data.Name);
+        setInner("ug", isi.data.Ug);
+        setInner("skill", isi.data.Skill);
+        setInner("rate", isi.data.Rate);
 }
